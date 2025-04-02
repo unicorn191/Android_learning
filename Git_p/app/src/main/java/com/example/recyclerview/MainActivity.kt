2f8b81a1e6a8d828eb764c6d609c7ea4ad958f8e
sirius03 <sirisirisha502@gmail.com>
2025-04-03 00:31:32 +0530

    removed unused imports.

--- Git_p/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ Git_p/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.recyclerview
 
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
