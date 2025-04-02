4598ea7598bc416ce630c54bea7bd7743df0c936
sirius03 <sirisirisha502@gmail.com>
2025-04-03 00:05:33 +0530

    Added click listeners.

--- Git_p/app/src/main/java/com/example/recyclerview/HobbiesAdapter.kt
+++ Git_p/app/src/main/java/com/example/recyclerview/HobbiesAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.recyclerview
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class HobbiesAdapter(private val context:Context, private val hobbies: List<Hobbies>):
@@ -28,9 +31,30 @@ class HobbiesAdapter(private val context:Context, private val hobbies: List<Hobb
     }
 
     inner class HobbiesViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
-            private var txvTitle: TextView = itemView.findViewById(R.id.txvTitle)
+        private var txvTitle: TextView = itemView.findViewById(R.id.txvTitle)
+        private var imgShare:ImageView = itemView.findViewById(R.id.imgShare)
+        var currentHobby:Hobbies? = null
+        var currentPosition:Int = 0
+
+        init {
+            itemView.setOnClickListener {
+                Toast.makeText(context,currentHobby!!.title, Toast.LENGTH_SHORT).show()
+            }
+
+            imgShare.setOnClickListener {
+                val msg:String = currentHobby!!.title
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, msg )
+                intent.type = "text/plain"
+                context.startActivity(Intent.createChooser(intent,"share to:"))
+            }
+        }
+
         fun setData(hobby: Hobbies?, pos:Int){
             txvTitle.text = hobby!!.title
+            this.currentHobby = hobby
+            this.currentPosition = pos
         }
     }
 }
