4af9bd01b8cf0b092157d85d38e993402d363705
sirius03 <sirisirisha502@gmail.com>
2025-04-04 17:11:49 +0530

    Adding Splash screen project

---
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.splashscreen
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+    }
+}
